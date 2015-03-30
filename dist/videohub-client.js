a5a1a4d7cafd837ad327791a022a2f79f968cb03
Andrew Kos <akos@theonion.com>
2015-03-30 14:32:48 -0500

    Version up to 0.1.0

--- dist/videohub-client.js
+++ dist/videohub-client.js
@@ -1,57 +1,99 @@
 // Source: src/videohub-client/videohub-api.js
-angular.module('VideohubClient.api', ['restangular', 'VideohubClient.settings'])
-  .factory('VideohubApi', function (Restangular, VIDEOHUB_API_BASE_URL, VIDEOHUB_SECRET_TOKEN) {
-    return Restangular.withConfig(function (RestangularConfigurer) {
-      RestangularConfigurer.setRequestSuffix('');
-      RestangularConfigurer.setBaseUrl(VIDEOHUB_API_BASE_URL);
-      RestangularConfigurer.setDefaultHeaders({
-        Authorization: 'Token ' + VIDEOHUB_SECRET_TOKEN
-      });
-      RestangularConfigurer.addResponseInterceptor(function (data, operation, what) {
-        if (what === 'search' && operation === 'post') {
-          var newData = {
-            count: data.count,
-            results: _.map(data.results, function (value) {
-              // extract some keywords for this video
-              var keywords = [];
-              if (value.channel) {
-                keywords.push(value.channel.name);
-                keywords.push(value.channel.description);
-              }
-              if (value.sponsor) {
-                keywords.push(value.sponsor.name);
-              }
-              if (value.series) {
-                keywords.push(value.series.name);
-                keywords.push(value.series.description);
-              }
-              if (value.season) {
-                keywords.push(value.season.name);
-                keywords.push(value.season.number);
-              }
-              if (value.tags) {
-                for (var i = 0; i < value.tags.length; i++) {
-                  keywords.push(value.tags[i]);
-                }
-              }
-              keywords = keywords.join(' ');
-              return {
-                id: value.id,
-                title: value.title,
-                description: value.description || '',
-                keywords: keywords,
-                image: null
-              };
-            })
-          };
-          return newData;
+angular.module('VideohubClient.api', [
+  'restmod',
+  'VideohubClient.settings'
+])
+  .factory('Video', function (restmod, VIDEOHUB_API_BASE_URL, VIDEOHUB_SECRET_TOKEN) {
+
+    var videosEndpoint = 'videos';
+    var searchEndpoint = videosEndpoint + '/search';
+
+    var gatherKeywords = function (record) {
+      // extract some keywords for this video
+      var keywords = [];
+      if (record.channel) {
+        keywords.push(record.channel.name);
+        keywords.push(record.channel.description);
+      }
+      if (record.sponsor) {
+        keywords.push(record.sponsor.name);
+      }
+      if (record.series) {
+        keywords.push(record.series.name);
+        keywords.push(record.series.description);
+      }
+      if (record.season) {
+        keywords.push(record.season.name);
+        keywords.push(record.season.number);
+      }
+      if (record.tags) {
+        for (var i = 0; i < record.tags.length; i++) {
+          keywords.push(record.tags[i]);
         }
-        return data;
-      });
+      }
+      record.keywords = keywords.join(' ');
+    };
+
+    var Video = restmod.model(videosEndpoint).mix({
+      $config: {
+        urlPrefix: VIDEOHUB_API_BASE_URL,
+        name: 'Video',
+        plural: 'Videos',
+        primaryKey: 'id'
+      },
+
+      title: {
+        init: ''
+      },
+      description: {
+        init: ''
+      },
+      keywords: {
+        init: []
+      },
+      image: {
+        init: null
+      },
+
+      $hooks: {
+        'before-request': function (_req) {
+          _req.headers = angular.extend(_req.headers, {
+            Authorization: 'Token ' + VIDEOHUB_SECRET_TOKEN
+          });
+        },
+        'after-fetch': function () {
+          gatherKeywords(this);
+        },
+        'after-fetch-many': function () {
+          _.each(this, function (record) {
+            gatherKeywords(record);
+          });
+        }
+      },
+
+      $extend: {
+        Model: {
+          $postSearch: function (params) {
+            return VideoSearch.$create(params).$asPromise()
+              .then(function (data) {
+                return data.results;
+              });
+          }
+        }
+      }
     });
-  })
-  .factory('VideohubVideoApi', function (VideohubApi) {
-    return VideohubApi.all('videos');
+
+    var VideoSearch = restmod.model(searchEndpoint).mix({
+      $config: {
+        urlPrefix: VIDEOHUB_API_BASE_URL
+      },
+
+      results: {
+        hasMany: 'Video'
+      }
+    });
+
+    return Video;
   });
 
 // Source: src/videohub-client/videohub-client.js
@@ -110,7 +152,7 @@ angular.module('VideohubClient.suggest.directive', [
         onSelect: '&',
         givenChannel: '@channel'
       },
-      controller: function ($scope, $q, VideohubVideoApi, BULBS_AUTOCOMPLETE_EVENT_KEYPRESS,
+      controller: function ($scope, $q, Video, BULBS_AUTOCOMPLETE_EVENT_KEYPRESS,
           VIDEOHUB_DEFAULT_CHANNEL) {
 
         $scope.channel = $scope.givenChannel || VIDEOHUB_DEFAULT_CHANNEL;
@@ -128,8 +170,8 @@ angular.module('VideohubClient.suggest.directive', [
                 channel: $scope.channel
               };
             }
-            VideohubVideoApi.all('search').post(params).then(function (data) {
-              defer.resolve(data.results);
+            Video.$postSearch(params).then(function (videos) {
+              defer.resolve(videos);
             });
           } else {
             defer.resolve([]);
