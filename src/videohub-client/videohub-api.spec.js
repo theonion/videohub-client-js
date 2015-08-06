8264fda95ab25b85c8a353cd720334f9d80d0c56
vforgione <v.forgione@gmail.com>
2015-08-06 00:25:24 -0500

    cleaning tests - getting closer i think

--- src/videohub-client/videohub-api.spec.js
+++ src/videohub-client/videohub-api.spec.js
@@ -10,13 +10,10 @@ describe('Factory: Video', function () {
   beforeEach(function () {
     module('VideohubClient.api');
 
-    inject(function (_$httpBackend_, _Video_, _VIDEOHUB_SECRET_TOKEN_,
-        VIDEOHUB_API_BASE_URL) {
+    inject(function (_$httpBackend_, _Video_, VIDEOHUB_API_BASE_URL) {
       $httpBackend = _$httpBackend_;
-      searchEndpoint = new RegExp(VIDEOHUB_API_BASE_URL + '/videos/search\/?$');
       singleEndpoint = new RegExp(VIDEOHUB_API_BASE_URL + '/videos/(\\d+)\/?$');
       Video = _Video_;
-      VIDEOHUB_SECRET_TOKEN = _VIDEOHUB_SECRET_TOKEN_;
     });
   });
 
@@ -39,7 +36,7 @@ describe('Factory: Video', function () {
     });
 
     it('searching for videos', function () {
-      Video.$postSearch();
+      Video.$search({});
 
       $httpBackend.expectPOST(searchEndpoint)
         .respond(function (method, url, data, headers) {
@@ -51,47 +48,6 @@ describe('Factory: Video', function () {
 
     afterEach(function () {
       expect(requestHeaders.Authorization).toBe('Token ' + VIDEOHUB_SECRET_TOKEN);
-    });
-  });
-
-  describe('should ensure channel is an object with a name property when', function () {
-
-    it('retrieving a single video', function () {
-      var name = 'some video blah blah';
-      var video = Video.$find(1);
-
-      $httpBackend.expectGET(singleEndpoint).respond(function () {
-        return [200, {
-          id: 123,
-          channel: {
-            name: name
-          }
-        }]
-      });
-      $httpBackend.flush();
-
-      expect(video.channel.name).toBe(name);
-    });
-
-    it('searching for videos', function () {
-      var name = 'some video blah blah';
-
-      var videos;
-      Video.$postSearch().then(function (results) {
-        videos = results;
-      });
-
-      $httpBackend.expectPOST(searchEndpoint).respond(function () {
-        return [200, {
-          results: [{
-            id: 123,
-            channel: name
-          }]
-        }]
-      });
-      $httpBackend.flush();
-
-      expect(videos[0].channel.name).toBe(name);
     });
   });
 });
